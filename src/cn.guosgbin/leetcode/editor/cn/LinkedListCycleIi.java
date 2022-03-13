0dcf45d7550fd2e3f23d3a650bcc2011966d33fe
Dylan Kwok <guosgbin@163.com>
2022-03-13 17:32:26 +0800

    feat(哈希表):142. 环形链表 II

--- src/cn.guosgbin/leetcode/editor/cn/LinkedListCycleIi.java
+++ src/cn.guosgbin/leetcode/editor/cn/LinkedListCycleIi.java
@@ -1,6 +1,7 @@
 package cn.guosgbin.leetcode.editor.cn;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 /**
@@ -63,13 +64,49 @@ public class Solution {
      * 假如指针 A 以1个结点的速度前进，指针B以2个结点的速度前进
      *
      * 假如
+     *    |------ a -----|
      *    O -> O -> O -> O -> O -> O
      *                     /       |
      *                   O <- O <- O
+     *
+     *    假如 快慢指针 在环内的某个结点相遇，
+     *    环入口到相遇点长度 b
+     *    相遇点到环入口长度 c
+     *
+     *    在环内相遇，那么快指针肯定比慢指针多走了 n 圈
+     *
+     *    假如快指针和慢指针走过的长度分别为 f，s
+     *    f = a + n(b + c) + b
+     *    s = a + b  (慢指针入环第一圈没走完的时候就会和快指针相遇)
+     *    因为 f = 2s， 即 a + n(b + c) + b = 2(a + b)
+     *                 得到 a = (n - 1)(b + c) + c
+     *
+     * 从相遇点到入环点的距离加上 n−1 圈的环长，恰好等于从链表头部到入环点的距离。
+     * 此时定义一个新指针，速度为1，此时慢指针继续前进，这两个指针相遇的地方就是环的入口
      */
     public ListNode detectCycle(ListNode head) {
-        // TODO-KWOK 待解
-        return null;
+        // 定义快慢指针
+        ListNode fast = head;
+        ListNode slow = head;
+        ListNode cur = head;
+        while (true) {
+            if (fast == null || fast.next == null) {
+                // 链表结束了,没有环
+                return null;
+            }
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) {
+                // 快慢指针相遇了
+               break;
+            }
+        }
+
+        while (cur != slow) {
+            cur = cur.next;
+            slow = slow.next;
+        }
+        return cur;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
