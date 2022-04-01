5acadc34a6cb8cc55048315863f27ab6cdf2207e
Dylan Kwok <guosgbin@163.com>
2022-04-01 22:20:36 +0800

    feat(二叉树): 94. 二叉树的中序遍历

--- README.md
+++ README.md
@@ -87,3 +87,4 @@
  # 二叉树
  -[144. 二叉树的前序遍历](https://leetcode-cn.com/problems/binary-tree-preorder-traversal/)
  -[145. 二叉树的后序遍历](https://leetcode-cn.com/problems/binary-tree-postorder-traversal/)
+ -[94. 二叉树的中序遍历](https://leetcode-cn.com/problems/binary-tree-inorder-traversal/)
---
+++ src/cn/guosgbin/leetcode/editor/cn/BinaryTreeInorderTraversal.java
@@ -0,0 +1,63 @@
+package cn.guosgbin.leetcode.editor.cn;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Dylan Kwok
+ * @date 2022-04-01 22:17:20
+ */
+public class BinaryTreeInorderTraversal{
+    public static void main(String[] args) {
+        Solution solution = new BinaryTreeInorderTraversal().new Solution();
+   }
+
+     public class TreeNode {
+         int val;
+         TreeNode left;
+         TreeNode right;
+         TreeNode() {}
+         TreeNode(int val) { this.val = val; }
+         TreeNode(int val, TreeNode left, TreeNode right) {
+             this.val = val;
+             this.left = left;
+             this.right = right;
+         }
+     }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        traversal(root, list);
+        return list;
+    }
+
+    public void traversal(TreeNode node, List<Integer> list) {
+        if (node == null) {
+            return;
+        }
+        traversal(node.left, list);
+        list.add(node.val);
+        traversal(node.right, list);
+
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
