c0f25e6b1e0ed06e53f3f6632f4f65b6c24a7171
Dylan Kwok <guosgbin@163.com>
2022-04-03 00:00:08 +0800

    feat(二叉树): 94. 二叉树的中序遍历 迭代法

--- src/cn/guosgbin/leetcode/editor/cn/BinaryTreeInorderTraversal.java
+++ src/cn/guosgbin/leetcode/editor/cn/BinaryTreeInorderTraversal.java
@@ -2,6 +2,7 @@ package cn.guosgbin.leetcode.editor.cn;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 /**
  * @author Dylan Kwok
@@ -42,11 +43,11 @@ public class BinaryTreeInorderTraversal{
  * }
  */
 class Solution {
-    public List<Integer> inorderTraversal(TreeNode root) {
-        List<Integer> list = new ArrayList<>();
-        traversal(root, list);
-        return list;
-    }
+//    public List<Integer> inorderTraversal(TreeNode root) {
+//        List<Integer> list = new ArrayList<>();
+//        traversal(root, list);
+//        return list;
+//    }
 
     public void traversal(TreeNode node, List<Integer> list) {
         if (node == null) {
@@ -56,6 +57,34 @@ class Solution {
         list.add(node.val);
         traversal(node.right, list);
 
+    }
+
+    /**
+     * 中序遍历迭代法
+     *
+     */
+    public List<Integer> inorderTraversal (TreeNode root){
+        Stack<TreeNode> stack = new Stack<>();
+        List<Integer> list = new ArrayList<>();
+        if (root == null) {
+            return list;
+        }
+        TreeNode cur = root;
+        while (cur != null || !stack.isEmpty()) {
+            if (cur != null) {
+                // 表示左子结点有数据
+                // 添加到栈
+                stack.add(cur);
+                cur = cur.left;
+            } else {
+                // 说明当前结点没有左子结点了，添加元素
+                TreeNode node = stack.pop();
+                list.add(node.val);
+                // 遍历右子树
+                cur = node.right;
+            }
+        }
+        return list;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
