b64ac9ced656fbbc6eccf51e1ad7f0ece3bf1ae8
Dylan Kwok <guosgbin@163.com>
2022-04-22 00:01:52 +0800

    feat(二叉树的最大深度):104.二叉树的最大深度

---
+++ src/cn/guosgbin/leetcode/editor/cn/SubtreeOfAnotherTree.java
@@ -0,0 +1,47 @@
+package cn.guosgbin.leetcode.editor.cn;
+ /**
+ * @author Dylan Kwok
+ * @date 2022-04-21 23:33:35
+ */
+public class SubtreeOfAnotherTree{
+    public static void main(String[] args) {
+        Solution solution = new SubtreeOfAnotherTree().new Solution();
+   }
+
+     public class TreeNode {
+         int val;
+         TreeNode left;
+         TreeNode right;
+         TreeNode() {}
+         TreeNode(int val) { this.val = val; }
+         TreeNode(int val, TreeNode left, TreeNode right) {
+             this.val = val;
+             this.left = left;
+             this.right = right;
+         }
+     }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
