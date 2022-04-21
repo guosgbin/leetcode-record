c0bcf2396de1da3ff55cbb32a38e21c8547dfdc0
Dylan Kwok <guosgbin@163.com>
2022-04-21 23:51:03 +0800

    feat(二叉树的最大深度):104.二叉树的最大深度

--- src/cn/guosgbin/leetcode/editor/cn/MaximumDepthOfBinaryTree.java
+++ src/cn/guosgbin/leetcode/editor/cn/MaximumDepthOfBinaryTree.java
@@ -41,34 +41,60 @@ public class MaximumDepthOfBinaryTree{
  * }
  */
 class Solution {
+//    /**
+//     * 最大深度 层次遍历
+//     */
+//    public int maxDepth(TreeNode root) {
+//        if (root == null) {
+//            return 0;
+//        }
+//        ArrayDeque<TreeNode> queue = new ArrayDeque<>();
+//        queue.offer(root);
+//
+//        // 层数
+//        int floor = 0;
+//        while (!queue.isEmpty()) {
+//            int size = queue.size();
+//            while (size > 0) {
+//                TreeNode node = queue.poll();
+//                if (node.left != null) {
+//                    queue.offer(node.left);
+//                }
+//                if (node.right != null) {
+//                    queue.offer(node.right);
+//                }
+//                size--;
+//            }
+//            // 每层的
+//            floor++;
+//        }
+//        return floor;
+//    }
+
     /**
-     * 最大深度 层次遍历
+     * 最大深度 递归法
+     *
+     * 使用中序遍历，返回最大深度值
+     *
+     * 参数值和返回值：
+     * 递归的出口：结点为空
+     * 每次递归的方法内做什么：
      */
     public int maxDepth(TreeNode root) {
-        if (root == null) {
+        return getdepth(root);
+    }
+
+    /**
+     * 中序遍历
+     */
+    public int getdepth(TreeNode node) {
+        if (node == null) {
             return 0;
         }
-        ArrayDeque<TreeNode> queue = new ArrayDeque<>();
-        queue.offer(root);
-
-        // 层数
-        int floor = 0;
-        while (!queue.isEmpty()) {
-            int size = queue.size();
-            while (size > 0) {
-                TreeNode node = queue.poll();
-                if (node.left != null) {
-                    queue.offer(node.left);
-                }
-                if (node.right != null) {
-                    queue.offer(node.right);
-                }
-                size--;
-            }
-            // 每层的
-            floor++;
-        }
-        return floor;
+        int leftdepth = getdepth(node.left); // 左
+        int rightdepth = getdepth(node.right); // 右
+        int depth = 1 + Math.max(leftdepth, rightdepth); // 中
+        return depth;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
