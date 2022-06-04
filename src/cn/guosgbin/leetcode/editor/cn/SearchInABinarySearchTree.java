72d5ea5922ad26d1bb862a56e0f86e575bd56b0c
Dylan Kwok <guosgbin@163.com>
2022-06-05 00:31:03 +0800

    feat(二叉树): 700. 二叉搜索树中的搜索

--- src/cn/guosgbin/leetcode/editor/cn/SearchInABinarySearchTree.java
+++ src/cn/guosgbin/leetcode/editor/cn/SearchInABinarySearchTree.java
@@ -1,5 +1,9 @@
 package cn.guosgbin.leetcode.editor.cn;
- /**
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
  * @author Dylan Kwok
  * @date 2022-06-04 23:00:29
  */
@@ -38,33 +42,55 @@ public class SearchInABinarySearchTree{
  * }
  */
 class Solution {
+//    /**
+//     * 递归法
+//     *
+//     * 方法的入参和返回值：
+//     * 递归的出口：结点为null
+//     * 单层逻辑：找出值相等的结点，如果相等直接返回。
+//     *          如果不相等，假如要找的结点大于当前结点，就递归右节点，
+//     *                    假如要找的结点小于当前结点，就递归左节点
+//     */
+//    public TreeNode searchBST(TreeNode root, int val) {
+//        if (root == null) {
+//            return null;
+//        }
+//
+//        if (root.val == val) {
+//            return root;
+//        }
+//
+//        TreeNode node = null;
+//        if (val > root.val && root.right != null) {
+//            node = searchBST(root.right, val);
+//        }
+//
+//        if (val < root.val && root.left != null) {
+//            node = searchBST(root.left, val);
+//        }
+//        return node;
+//    }
+
+
     /**
-     * 递归法
-     *
-     * 方法的入参和返回值：
-     * 递归的出口：结点为null
-     * 单层逻辑：找出值相等的结点，如果相等直接返回。
-     *          如果不相等，假如要找的结点大于当前结点，就递归右节点，
-     *                    假如要找的结点小于当前结点，就递归左节点
+     * 迭代法
      */
     public TreeNode searchBST(TreeNode root, int val) {
         if (root == null) {
             return null;
         }
 
-        if (root.val == val) {
-            return root;
+        TreeNode node = root;
+        while (node != null) {
+            if (node.val == val) {
+                return node;
+            }else if (val < node.val) {
+                node = node.left;
+            } else {
+                node = node.right;
+            }
         }
-
-        TreeNode node = null;
-        if (val > root.val && root.right != null) {
-            node = searchBST(root.right, val);
-        }
-
-        if (val < root.val && root.left != null) {
-            node = searchBST(root.left, val);
-        }
-        return node;
+        return null;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
