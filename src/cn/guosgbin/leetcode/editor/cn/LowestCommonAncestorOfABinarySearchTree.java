37105f96c1ec86ff8df3e8735f20207afacccda3
Dylan Kwok <guosgbin@163.com>
2022-06-20 22:28:38 +0800

    feat(二叉树): 235. 二叉搜索树的最近公共祖先

--- src/cn/guosgbin/leetcode/editor/cn/LowestCommonAncestorOfABinarySearchTree.java
+++ src/cn/guosgbin/leetcode/editor/cn/LowestCommonAncestorOfABinarySearchTree.java
@@ -27,37 +27,53 @@ public class LowestCommonAncestorOfABinarySearchTree{
  */
 
 class Solution {
+//    /**
+//     * 二叉搜索树的最近公共祖先
+//     *
+//     * 迭代法
+//     *
+//     * 二叉搜索树的特点，对于结点， 左 < 中 < 右
+//     */
+//    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+//        if (root == null) {
+//            return null;
+//        }
+//
+//        // 二叉搜索树有序，所以需要找到 [p.val, q.val] 或者 [q.val, p.val] 之间的数就行了
+//        // 前序遍历 中左右
+//
+//        if (p.val < root.val && q.val < root.val) {
+//            // 说明两个节点在 root 的左边
+//            TreeNode left = lowestCommonAncestor(root.left, p, q);
+//            if (left != null) {
+//                return left;
+//            }
+//        }
+//
+//        if (p.val > root.val && q.val > root.val) {
+//            // 说明两个节点在 root 的右边
+//            TreeNode right = lowestCommonAncestor(root.right, p, q);
+//            if (right != null) {
+//                return right;
+//            }
+//        }
+//        return root;
+//    }
+
     /**
-     * 二叉搜索树的最近公共祖先
-     *
      * 迭代法
-     *
-     * 二叉搜索树的特点，对于结点， 左 < 中 < 右
      */
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if (root == null) {
-            return null;
-        }
-
-        // 二叉搜索树有序，所以需要找到 [p.val, q.val] 或者 [q.val, p.val] 之间的数就行了
-        // 前序遍历 中左右
-
-        if (p.val < root.val && q.val < root.val) {
-            // 说明两个节点在 root 的左边
-            TreeNode left = lowestCommonAncestor(root.left, p, q);
-            if (left != null) {
-                return left;
+        while (root != null) {
+            if (p.val < root.val && q.val < root.val) {
+                root = root.left;
+            } else if (p.val > root.val && q.val > root.val) {
+                root = root.right;
+            } else {
+                return root;
             }
         }
-
-        if (p.val > root.val && q.val > root.val) {
-            // 说明两个节点在 root 的右边
-            TreeNode right = lowestCommonAncestor(root.right, p, q);
-            if (right != null) {
-                return right;
-            }
-        }
-        return root;
+        return null;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
